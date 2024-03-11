ab010b074b973645c926827b823bb566c342ff9b
blazhkevych.v <blazhkevych.v@gmail.com>
2024-03-11 17:22:47 +0200

    # - dial

--- app/src/main/java/com/example/caller/MainActivity.java
+++ app/src/main/java/com/example/caller/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.caller.listeners.BtnCallClickListener;
-import com.example.caller.listeners.BtnDialClickListener;
+/*import com.example.caller.listeners.BtnDialClickListener;*/
 import com.example.caller.listeners.BtnSendSMSClickListener;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Set the click listener for the button to dial the number
-        findViewById(R.id.button_dial).setOnClickListener(new BtnDialClickListener(this));
+/*        findViewById(R.id.button_dial).setOnClickListener(new BtnDialClickListener(this));*/
         // Set the click listener for the button to call the number
         findViewById(R.id.button_call).setOnClickListener(new BtnCallClickListener(this));
         // Set the click listener for the button to send an SMS to the number
--- app/src/main/java/com/example/caller/listeners/BtnDialClickListener.java
+++ app/src/main/java/com/example/caller/listeners/BtnDialClickListener.java
@@ -1,3 +1,4 @@
+/*
 package com.example.caller.listeners;
 
 import android.app.Activity;
@@ -20,3 +21,4 @@ public class BtnDialClickListener implements View.OnClickListener {
         activity.startActivity(intent);
     }
 }
+*/
